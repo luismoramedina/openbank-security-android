ee3c776b1c96c9dd18f0f36c60f165d6448d4b6b
Javier Sanz <javier.sanz.salesa@gmail.com>
2015-11-20 04:12:33 +0100

    Se elimina la activity DocumentActivity

--- app/src/main/java/com/example/lmora/myapplication/ResultActivity.java
+++ app/src/main/java/com/example/lmora/myapplication/ResultActivity.java
@@ -16,6 +16,15 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.bouncycastle.asn1.cms.ContentInfo;
+import org.bouncycastle.cms.CMSException;
+import org.bouncycastle.cms.CMSProcessable;
+import org.bouncycastle.cms.CMSSignedData;
+import org.bouncycastle.cms.SignerInformation;
+import org.bouncycastle.cms.SignerInformationStore;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.util.Store;
+import org.bouncycastle.x509.X509Store;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -23,9 +32,17 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.security.cert.CertStore;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 
 public class ResultActivity extends AppCompatActivity {
+
+    private static final String DOCUMENT_ENDPOINT_URI = "http://172.26.0.132:8080/document?id=blah";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,11 +69,13 @@ public class ResultActivity extends AppCompatActivity {
     }
 
     private class DownloadDocTask extends AsyncTask<URL, Integer, String> {
+
         private final View view;
 
         public DownloadDocTask(View view) {
             this.view = view;
         }
+
 
         protected String doInBackground(URL... urls) {
             // Create connection objects
@@ -71,8 +90,6 @@ public class ResultActivity extends AppCompatActivity {
             return str;
         }
 
-
-
         protected void onProgressUpdate(Integer... progress) {
             //setProgressPercent(progress[0]);
         }
@@ -83,9 +100,13 @@ public class ResultActivity extends AppCompatActivity {
                 JSONObject object = new JSONObject(result);
                 try {
                     String data = (String) object.get("data");
-                    byte[] decode = Base64.decode(data, Base64.DEFAULT);
-                    fileData = new String(decode);
-                }catch(Exception e){
+                    if (!verifyPKCS7Signature(data)) {
+                        throw new IllegalArgumentException("Document signature verification is KO");
+                    }
+                    fileData = extractDocumentData(data);
+//                    byte[] decode = Base64.decode(data, Base64.DEFAULT);
+//                    fileData = new String(decode);
+                } catch (Exception e) {
                     fileData = (String) object.get("result");
                     Snackbar.make(view, "Operation locked. Checks your Latch", Snackbar.LENGTH_LONG)
                             .setAction("Action", null).show();
@@ -100,8 +121,6 @@ public class ResultActivity extends AppCompatActivity {
 
     }
 
-    private static final String DOCUMENT_ENDPOINT_URI_DROPBOX = "https://dl.dropboxusercontent.com/u/1368598/data.txt";
-    private static final String DOCUMENT_ENDPOINT_URI = "http://172.26.0.124/document?id=blah";
     private String doHttpConnection() throws URISyntaxException, IOException {
         // Create connection objects
 
@@ -116,5 +135,43 @@ public class ResultActivity extends AppCompatActivity {
         return IOUtils.toString(response.getEntity().getContent());
     }
 
+    private String extractDocumentData(String b64SignedData) throws Exception {
+        CMSSignedData decodedSignedData = unmarshallPkcs7Data(b64SignedData);
 
+        CMSProcessable signedData = decodedSignedData.getSignedContent();
+        byte[] signedContent = (byte[]) signedData.getContent();
+        return new String(signedContent, "UTF-8");
+    }
+
+    private boolean verifyPKCS7Signature(String b64SignedData) throws Exception {
+        CMSSignedData decodedSignedData = unmarshallPkcs7Data(b64SignedData);
+
+        Object[] firstSignerIdAndCertificate = getFirstSignerIdAndCertificate(decodedSignedData);
+        SignerInformation signer = (SignerInformation) firstSignerIdAndCertificate[0];
+        X509Certificate certificate = (X509Certificate) firstSignerIdAndCertificate[1];
+
+        return signer.verify(certificate.getPublicKey(), "BC");
+    }
+
+    private CMSSignedData unmarshallPkcs7Data(String pkcs7DataB64Encoded) throws Exception {
+        byte[] pkcs7Bytes = Base64.decode(pkcs7DataB64Encoded, Base64.DEFAULT);
+
+        // Se crea un objeto SignedData con los datos firmados a partir de los bytes recibidos.
+        return new CMSSignedData(pkcs7Bytes);
+    }
+
+    private Object[] getFirstSignerIdAndCertificate(CMSSignedData signedData) throws Exception {
+
+        CertStore store = signedData.getCertificatesAndCRLs("Collection", "BC");
+        SignerInformationStore signers = signedData.getSignerInfos();
+        Collection signerCollection = signers.getSigners();
+        Iterator it = signerCollection.iterator();
+        SignerInformation signer = (SignerInformation) it.next();
+        Collection certCollection = store.getCertificates(signer.getSID());
+        Iterator certIt = certCollection.iterator();
+
+        Certificate resultCert = (Certificate) certIt.next();
+
+        return new Object[]{signer, resultCert};
+    }
 }
